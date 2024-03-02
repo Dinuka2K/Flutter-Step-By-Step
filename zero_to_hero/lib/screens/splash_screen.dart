6f0352eb715b725fbee4621829ce7aca3187f581
Dinuka2K <dinuka_2020693@fot.ruh.ac.lk>
2024-03-02 09:11:02 +0530

    create flutter project
    create main.dart
    create splashScreen.dart
    then home page

---
+++ zero_to_hero/lib/screens/splash_screen.dart
@@ -0,0 +1,23 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:zero_to_hero/screens/home_page.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: const Column(
+        children: [
+          FlutterLogo(size: 100),
+          SizedBox(height: 20),
+          Text('Zero to Hero', style: TextStyle(fontSize: 24)),
+        ],
+      ),
+      nextScreen: HomePage(),
+      splashIconSize: 400,
+      backgroundColor: Color.fromARGB(255, 107, 159, 248),
+    );
+  }
+}
