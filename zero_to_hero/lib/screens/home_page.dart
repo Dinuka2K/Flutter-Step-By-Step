5cf37a3ea2c882718228da5e18e917c70b4ed89c
Dinuka2K <dinuka_2020693@fot.ruh.ac.lk>
2024-05-03 15:46:38 +0530

    create sign up and connect with firebase

--- zero_to_hero/lib/screens/home_page.dart
+++ zero_to_hero/lib/screens/home_page.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:zero_to_hero/auth/auth_service.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
+  void logout() {
+    final _auth = AuthService();
+    _auth.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,6 +15,12 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Zero to Hero'),
         backgroundColor: Colors.blue,
+        actions: [
+          IconButton(
+            onPressed: logout,
+            icon: Icon(Icons.logout),
+          ),
+        ],
       ),
       body: const Center(
         child: Text('Welcome to Zero to Hero'),
